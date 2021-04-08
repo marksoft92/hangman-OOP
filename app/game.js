da80bdfac5b9bfc1badaf1ac64fa8c86a2bff374
Dawid Bienkowski <marksoft1992@gmail.com>
2021-04-08 20:26:21 +0200

    draw letters

--- app/game.js
+++ app/game.js
@@ -1,18 +1,40 @@
 class Game {
+
+    quotes = [{
+        text: 'pan tadeusz',
+        category: 'utwór literacki'
+    }, {
+        text: 'matrix',
+        category: 'Film'
+    }];
+
     constructor({ lettersWrapper, outputWrapper, wordWrapper, categoryWrapper }) {
         this.lettersWrapper = lettersWrapper;
         this.outputWrapper = outputWrapper;
         this.wordWrapper = wordWrapper;
         this.categoryWrapper = categoryWrapper;
     }
+    quess(letters) {
+        console.log(letters)
+
+    }
+    drawLetters() {
+        for (let i = 10; i < 36; i++) {
+            const label = i.toString(36);
+            const button = document.createElement('button');
+            button.innerHTML = label;
+            button.addEventListener('click', () => this.quess(label));
+            this.lettersWrapper.appendChild(button);
+        }
+    }
     start() {
-        console.log('x')
+        this.drawLetters()
     }
 }
 const game = new Game({
-    lettersWrapper: document.getElementById('letters');
-    outputWrapper: document.getElementById('output');
-    wordWrapper: document.getElementById('word');
-    categoryWrapper: document.getElementById('letters');
+    lettersWrapper: document.getElementById('letters'),
+    outputWrapper: document.getElementById('output'),
+    wordWrapper: document.getElementById('word'),
+    categoryWrapper: document.getElementById('letters')
 });
 game.start()
