72a15559af397b747f51f4b7db90715e588c48f9
Dawid Bienkowski <marksoft1992@gmail.com>
2021-04-08 19:50:18 +0200

    add wrappers

--- app/game.js
+++ app/game.js
@@ -1,7 +1,18 @@
 class Game {
+    constructor({ lettersWrapper, outputWrapper, wordWrapper, categoryWrapper }) {
+        this.lettersWrapper = lettersWrapper;
+        this.outputWrapper = outputWrapper;
+        this.wordWrapper = wordWrapper;
+        this.categoryWrapper = categoryWrapper;
+    }
     start() {
         console.log('x')
     }
 }
-const game = new Game();
+const game = new Game({
+    lettersWrapper: document.getElementById('letters');
+    outputWrapper: document.getElementById('output');
+    wordWrapper: document.getElementById('word');
+    categoryWrapper: document.getElementById('letters');
+});
 game.start()
